e215ef2eba061cc42fd922f76e5faf98bf852f89
isaactobiloba <isaactobiloba7@gmail.com>
2023-09-14 16:30:06 +0100

    Modify seeders (foreign key checks, truncate db)

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -2,16 +2,23 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use Database\Seeders\Traits\ManageForeignKeyChecks;
+use Database\Seeders\Traits\TruncateTable;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class CommentSeeder extends Seeder
 {
+    use TruncateTable, ManageForeignKeyChecks;
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        $this->disableForeignKeys();
+        $this->truncate('comments');
+        Comment::factory(3)->create();
+        $this->enableForeignKeys();
     }
 }
--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,16 +2,23 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use Database\Seeders\Traits\ManageForeignKeyChecks;
+use Database\Seeders\Traits\TruncateTable;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PostSeeder extends Seeder
 {
+    use TruncateTable, ManageForeignKeyChecks;
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        $this->disableForeignKeys();
+        $this->truncate('posts');
+        Post::factory(3)->untitled()->create();
+        $this->enableForeignKeys();
     }
 }
